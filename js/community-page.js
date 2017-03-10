87148d9d297ccf34552d2bd52b67093963378361
Christin Westermann <christin.westermann@immobilienscout24.de>
2017-03-10 10:06:06 +0100

    removed comments and added origin flags to profiles

--- js/community-page.js
+++ js/community-page.js
@@ -3,55 +3,65 @@ var people = [
         name: "Raluca Idor",
         title: "Talen Acquisition Manager",
         company: "IS24",
-        img: "raluca.jpeg"
+        img: "raluca.jpeg",
+        flag: "ro"
     },
     katrin = {
         name: "Katrin Kern",
         title: "Engineering Manager",
         company: "IS24",
-        img: "christin.jpeg"
+        img: "christin.jpeg",
+        flag: "de"
     },
     sebastian = {
         name: "Sebastian Nachtigall",
         title: "Engineering Manager",
         company: "IS24",
-        img: "sebastian.jpeg"
+        img: "sebastian.jpeg",
+        flag: "de"
     },
     gro = {
         name: "Gro Geert-Jørgensen",
         title: "Agile Coach",
         company: "AS24",
-        img: "gro.png"
+        img: "gro.png",
+        flag: "de"
     },
     pavlo = {
         name: "Pavlo Voznenko",
         title: "Head of Technology",
         company: "AS24",
-        img: "pavlo.png"
+        img: "pavlo.png",
+        flag: "de"
     },
     myrtha = {
         name: "Myrtha Fuog",
         title: "Junior Fullstack Developer",
         company: "IS24",
-        img: "pavlo.png"
+        img: "pavlo.png",
+        flag: "ch"
+        
     },
     britta = {
         name: "Britta Reiners",
         title: "Agile Tester",
         company: "AS24",
-        img: "britta.png"
+        img: "britta.png",
+        flag: "de"
     },
     nicole = {
         name: "Nicole Nenova",
         title: "Working Student",
         company: "IS24",
-        img: "nicole.jpg"
+        img: "nicole.jpg",
+        flag: "de"
     },
     christin = {
         name: "Christin Westermann",
         title: "Software Engineer",
         company: "IS24",
-        img: "christin.jpeg"
+        img: "christin.jpeg",
+        flag: "de"
     }
 ];
 
@@ -66,19 +76,14 @@ var secondRowGridClass = calculateCssGridClassForRow(numberOfPeopleSecondRow);
 var peopleFirstRow = people.slice(0, numberOfPeopleFirstRow);
 var peopleSecondRow = people.slice(numberOfPeopleFirstRow, people.length);
 
-console.log("numberOfPeopleFirstRow:" + numberOfPeopleFirstRow);
-console.log("numberOfPeopleSecondRow:" + numberOfPeopleSecondRow);
-console.log("firstRowGridClass:" + firstRowGridClass);
-console.log("secondRowGridClass:" + secondRowGridClass);
-console.log("peopleFirstRow:" + peopleFirstRow);
-console.log("peopleSecondRow:" + peopleSecondRow);
-
-
 peopleFirstRow.forEach(function (value, i) {
     $("#profiles-desk1").append(
         "<div class=\"grid-item " + firstRowGridClass + " align-center\">" +
             "<img src=\"assets/" + value.img + "\" class=\"circular--square profile\">" + 
-            "<p class=\"font-s font-italic font-bold\">" + value.name + "</p>" +
+            "<p class=\"font-s font-italic font-bold\">" +                     
+                "<span class=\"flag-icon flag-icon-" + value.flag + "\"></span>  " +  
+                value.name +                 
+            "</p>" +
             "<p class=\"font-s font-italic\">" + value.company + ", " + value.title + "</p>" +
         "</div>");
 });  
@@ -87,7 +92,10 @@ peopleSecondRow.forEach(function (value, i) {
     $("#profiles-desk2").append(
         "<div class=\"grid-item " + secondRowGridClass + " align-center\">" +
             "<img src=\"assets/" + value.img + "\" class=\"circular--square profile\">" + 
-            "<p class=\"font-s font-italic font-bold\">" + value.name + "</p>" +
+            "<p class=\"font-s font-italic font-bold\">" +                     
+                "<span class=\"flag-icon flag-icon-" + value.flag + "\"></span>  " +  
+                value.name +                 
+            "</p>" +
             "<p class=\"font-s font-italic\">" + value.company + ", " + value.title + "</p>" +
         "</div>");
 });
